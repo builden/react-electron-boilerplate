226bd70253dfce33f8cf78f875fbbaa97997d7ab
builden <billdeng@qq.com>
2017-10-24 11:03:48 +0800

    dynamic import start page

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,25 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const webpack = require('webpack');
+const program = require('commander');
+const version = require('./package.json').version;
+
+program
+  .version(version)
+  .option('-s, --start <name>', 'start page')
+  .option('-w, --web', 'show at web')
+  .parse(process.argv);
 
 module.exports = function override(config, env) {
-  config.target = 'electron-renderer';
+  if (!program.web) config.target = 'electron-renderer';
 
   config = injectBabelPlugin('transform-decorators-legacy', config);
 
-  const version = require('./package.json').version;
   config.plugins.push(
     new webpack.DefinePlugin({
-      'process.env': { VERSION: JSON.stringify(version) },
+      'process.env': {
+        VERSION: JSON.stringify(version),
+        START_PAGE: JSON.stringify(program.start),
+      },
     })
   );
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+async function start() {
+  const startPage = process.env.START_PAGE || 'App';
+  try {
+    const StartComponent = (await import(`./${startPage}`)).default;
+    ReactDOM.render(<StartComponent />, document.getElementById('root'));
+  } catch (e) {
+    const App = (await import('./App')).default;
+    ReactDOM.render(<App />, document.getElementById('root'));
+  }
+}
+start();
+
 registerServiceWorker();
