04d3fa0810a4e8ac7aa5c481f704619c9f35f1d7
builden <billdeng@qq.com>
2017-11-09 14:49:57 +0800

    sub-child files move to electron-src

--- src/ui/editor-extensions/lua/provider-child.js
+++ src/ui/editor-extensions/lua/provider-child.js
@@ -1,10 +1,13 @@
 import { remote } from 'electron';
+import path from 'path';
 
 const forkChild = remote.require('./fork-child');
 
 let child = null;
 
 export function request(channel, body) {
-  if (!child) child = forkChild('./src/ui/editor-extensions/lua/sub-child.js');
+  const subChildPath = path.join(remote.app.getAppPath(), './electron-src/editor-extensions/lua/sub-child.js');
+  console.log('subChildPath', subChildPath);
+  if (!child) child = forkChild(subChildPath);
   return child.request(channel, body);
 }
