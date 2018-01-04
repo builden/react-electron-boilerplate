6568573da92eb8b670f8cfb5320c882f0a24f092
builden <billdeng@qq.com>
2018-01-04 23:31:33 +0800

    mock openBrowser at config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -20,7 +20,10 @@ program
   .parse(process.argv);
 
 function injectAntd(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  config = injectBabelPlugin(
+    ['import', { libraryName: 'antd', style: true }],
+    config
+  );
   config = rewireLess.withLoaderOptions({
     modifyVars: {
       '@font-size-base': '14px',
@@ -61,14 +64,22 @@ module.exports = function override(config, env) {
     config.target = 'electron-renderer';
   } else {
     Object.assign(config.resolve.alias, { electron: electron });
+
+    require('react-dev-utils/openBrowser');
+    const openBrowserPath = require.resolve('react-dev-utils/openBrowser');
+    require.cache[openBrowserPath].exports = url => {};
   }
 
   if (program.index)
-    config.entry[config.entry.length - 1] = path.join(__dirname, `./src/TestIndex/${program.index}.js`);
+    config.entry[config.entry.length - 1] = path.join(
+      __dirname,
+      `./src/TestIndex/${program.index}.js`
+    );
 
   if (inDev) {
     const loader = getBabelLoader(config.module.rules);
-    if (loader.options.presets) loader.options.presets[0] = 'babel-preset-react';
+    if (loader.options.presets)
+      loader.options.presets[0] = 'babel-preset-react';
     config = injectBabelPlugin('syntax-dynamic-import', config);
     config = injectBabelPlugin('transform-object-rest-spread', config);
     config = injectBabelPlugin('transform-class-properties', config);
